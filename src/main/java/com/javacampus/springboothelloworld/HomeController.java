1d689770155aa3dbfc866fb3003afeb8fddfee00
choiguenho <choiguenho@gmail.com>
2022-12-25 21:39:25 +0900

    springboot helloworld project created

---
+++ src/main/java/com/javacampus/springboothelloworld/HomeController.java
@@ -0,0 +1,13 @@
+package com.javacampus.springboothelloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String index() {
+		return "Hello World with Spring Boot";
+	}
+}
